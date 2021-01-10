2e5d73095f3d9eb2ad60e324881be414fcfad999
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-11 06:47:20 +0900

    add test for amari value repeated, shou

--- arc-111/a/main.cpp
+++ arc-111/a/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <string>
 #include <set>
 #include <algorithm>
 #include <cassert>
@@ -10,6 +11,28 @@ const bool debug = false;
 #define ALL(x) x.begin(), x.end()
 #define int long long
 
+// for debug
+void show_vector(/*const ref in*/ const vector<int> &vec, const string &label)
+{
+    cout << "[" << label << "]: " << endl;
+    for (auto v : vec)
+    {
+        cout << v << ",";
+    }
+    cout << endl;
+}
+// for test
+void assert_vec(/*const ref in*/ const vector<int> &target, /*const ref in*/ const vector<int> &answer)
+{
+    assert(target.size() == answer.size());
+    for (int i = 0;
+         i < target.size();
+         ++i)
+    {
+        assert(target[i] == answer[i]);
+    }
+}
+// ----------
 void shou_and_repeated(const int M,
                        /*ref out*/ vector<int> &shou,
                        /*ref out*/ vector<int> &amari_vector,
@@ -70,6 +93,98 @@ void shou_and_repeated(const int M,
             repeated.push_back(what_is_shou_for_amari[*v]);
         }
     }
+}
+
+void test_shou_and_repeated()
+{
+    // 88
+    vector<int> shou_88;
+    vector<int> amari_88;
+    vector<int> what_amari_88;
+    vector<int> repeated_88;
+    cout << "--- 88 --- " << endl;
+    shou_and_repeated(88,
+                      /*ref out*/ shou_88,
+                      /*ref out*/ amari_88,
+                      /*ref out*/ what_amari_88,
+                      /*ref out*/ repeated_88
+
+    );
+    show_vector(shou_88, "shou88");
+    show_vector(amari_88, "amari88");
+    show_vector(what_amari_88, "what amari88");
+    show_vector(repeated_88, "repeated_88");
+    assert_vec(shou_88, vector<int>{
+                            0,
+                            1,
+                            1,
+                            3,
+                            6,
+                        });
+    assert_vec(repeated_88, vector<int>{3, 6});
+
+    // 17
+    vector<int> shou_17;
+    vector<int> amari_17;
+    vector<int> what_amari_17;
+    vector<int> repeated_17;
+    cout << "--- 17 --- " << endl;
+    shou_and_repeated(17,
+                      /*ref out*/ shou_17,
+                      /*ref out*/ amari_17,
+                      /*ref out*/ what_amari_17,
+                      /*ref out*/ repeated_17
+
+    );
+    show_vector(shou_17, "shou17");
+    show_vector(amari_17, "amari17");
+    show_vector(what_amari_17, "what amari17");
+    show_vector(repeated_17, "repeated_17");
+    assert_vec(shou_17, vector<int>{0, 5, 8, 8, 2, 3, 5, 2, 9, 4, 1, 1, 7, 6, 4, 7});
+    assert_vec(repeated_17, vector<int>{0, 5, 8, 8, 2, 3, 5, 2, 9, 4, 1, 1, 7, 6, 4, 7});
+
+    // 23
+    vector<int> shou_23;
+    vector<int> amari_23;
+    vector<int> what_amari_23;
+    vector<int> repeated_23;
+    cout << "--- 23 --- " << endl;
+    shou_and_repeated(23,
+                      /*ref out*/ shou_23,
+                      /*ref out*/ amari_23,
+                      /*ref out*/ what_amari_23,
+                      /*ref out*/ repeated_23
+
+    );
+    show_vector(shou_23, "shou23");
+    show_vector(amari_23, "amari23");
+    show_vector(what_amari_23, "what amari23");
+    show_vector(repeated_23, "repeated_23");
+    assert_vec(shou_23, vector<int>{0, 4, 3, 4, 7, 8, 2, 6, 0, 8, 6, 9, 5, 6, 5, 2, 1, 7, 3, 9, 1, 3});
+    assert_vec(repeated_23, vector<int>{
+                                0,
+                                4,
+                                3,
+                                4,
+                                7,
+                                8,
+                                2,
+                                6,
+                                0,
+                                8,
+                                6,
+                                9,
+                                5,
+                                6,
+                                5,
+                                2,
+                                1,
+                                7,
+                                3,
+                                9,
+                                1,
+                                3,
+                            });
 }
 
 // repeated を割ったループ
@@ -180,11 +295,13 @@ int service(const int N, const int M)
     */
 }
 
-signed main(int argc, char *argv[])
+signed main(signed argc, char *argv[])
 {
-    long long N;
-    int M;
-    cin >> N >> M;
+    test_shou_and_repeated();
 
-    service(N, M);
+    // long long N;
+    // int M;
+    // cin >> N >> M;
+
+    // service(N, M);
 }
