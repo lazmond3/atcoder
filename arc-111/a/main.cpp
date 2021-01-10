543a207cfd957bb2a3be250e9008062711ec771e
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-11 06:36:45 +0900

    テスト戦略がわからん...

--- arc-111/a/main.cpp
+++ arc-111/a/main.cpp
@@ -72,13 +72,83 @@ void shou_and_repeated(const int M,
     }
 }
 
-// return times
+// repeated を割ったループ
+// return amari
 int repeated_and_amari_cycle(const int M,
                              /*ref out*/ vector<int> &amari_vector,
-                             /*const ref in*/ const vector<int> &repeated)
+                             /*const ref in*/ const vector<int> &repeated_with_amari) // repeated とは限られず、先頭にあまりが付加した形になる。
 {
+    int i = 0;
+    int target = repeated_with_amari[i++];
+    int amari = 0;
+    while (i < repeated_with_amari.size())
+    {
+        target = target * 10 + repeated_with_amari[i++];
+        if (target >= M)
+        {
+            int shou_one = target / M;
+            int amari_one = target % M;
+            target = amari_one;
+            // shou.push_back(shou_one);
+        }
+        else
+        {
+            // shou.push_back(0);
+            // ❌ これ毎回間違える--!! _ は不要。
+            // target = target * 10 + warareru[i++];
+            continue;
+        }
+    }
+    if (target >= M)
+    {
+        int shou_one = target / M;
+        int amari_one = target % M;
+        target = amari_one;
+        // shou.push_back(shou_one);
+    }
+
+    // amari = target; // これって b 未満だよね？
+    assert(target < M);
+    return amari;
 }
 
+// repeated を何回連結させるとあまりがゼロになるか。
+int repeated_mod_times(const int M,
+                       /*ref out*/ vector<int> &amari_vector,
+                       /*const ref in*/ const vector<int> &repeated)
+{
+    int times = 1;
+    int amari = 0;
+
+    amari = repeated_and_amari_cycle(/*const int*/ M,
+                                     /*ref out*/ amari_vector,
+                                     /*const ref in*/ repeated);
+
+    vector<int> not_used_amari_vector;
+    amari_vector.push_back(amari);
+    while (amari != 0)
+    {
+        vector<int> next_repeated;
+        int _amari = amari;
+
+        while (_amari != 0)
+        {
+            next_repeated.push_back(_amari % 10);
+            _amari = _amari / 10;
+        }
+
+        for (auto r : repeated)
+            next_repeated.push_back(r);
+
+        amari = repeated_and_amari_cycle(
+            /*const int*/ M,
+            /*ref out*/ not_used_amari_vector,
+            /*const ref in*/ next_repeated);
+        amari_vector.push_back(amari);
+        times += 1;
+    }
+    return times;
+}
 int service(const int N, const int M)
 {
     vector<int> shou, repeated, what_is_shou_for_amari;
@@ -97,6 +167,11 @@ int service(const int N, const int M)
 
     // shou を M で割ったあまりはいくつになる？
     // 結局何桁ある？
+    /*
+        1 / 17 = 0.0588235294117647]
+        shou = 0,5,8,8,2,3,5,2,9,4,1,1,7,6,4,7
+        なので、N = 16 で最初のshouを使える。
+    */
     /*
         shou を M で割ったあまり
         => 1 / 88 = 0.01136 + 36 となる。
