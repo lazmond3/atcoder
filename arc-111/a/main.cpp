154820935ab0fb3cfa475d805a5f2c8386729074
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-11 06:18:01 +0900

    update main.cpp

--- arc-111/a/main.cpp
+++ arc-111/a/main.cpp
@@ -1,30 +1,115 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cassert>
 using namespace std;
+const bool debug = false;
 #define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define ALL(x) x.begin(), x.end()
+#define int long long
 
-long long pow(long long a, long long p)
+void shou_and_repeated(const int M,
+                       /*ref out*/ vector<int> &shou,
+                       /*ref out*/ vector<int> &amari_vector,
+                       /*ref out*/ vector<int> &what_is_shou_for_amari,
+                       /*ref out*/ vector<int> &repeated)
 {
-    if (p == 0)
-        return 1;
-    return a * pow(a, p - 1);
+    set<int> already_seen_amari;
+    int now_num = M;
+    int amari_now = 1;
+    int last_amari = -1;
+
+    what_is_shou_for_amari.resize(M + 1);
+    shou.resize(0);
+    amari_vector.resize(0);
+    repeated.resize(0);
+
+    while (true)
+    {
+        // amari_now は M 以下の状態でここに来る（はず）
+        if (already_seen_amari.find(amari_now) != already_seen_amari.end())
+        {
+            if (debug)
+            {
+                cout << "again amari is : " << amari_now << ", and 商 is " << what_is_shou_for_amari[amari_now] << endl;
+            }
+            last_amari = amari_now;
+            break;
+        }
+
+        already_seen_amari.insert(amari_now);
+        amari_vector.push_back(amari_now);
+
+        // ケタあげする。
+        amari_now *= 10;
+
+        // このループ 1回 で 10 を 1回 かける
+        if (amari_now < now_num)
+        {
+            // よくミスる: ここで 10を かける必要はない。
+            what_is_shou_for_amari[amari_now] = 0;
+            shou.push_back(0);
+            continue;
+        }
+
+        shou.push_back(amari_now / M);
+        what_is_shou_for_amari[amari_now / 10] = amari_now / M;
+        amari_now = amari_now % M;
+    }
+    // what_is_shou_for_amari, shou の作成。
+    // last_amari が 再度見たあまりとして _ に出てくる。
+
+    // repeated の作成
+    auto p = std::find(ALL(amari_vector), last_amari);
+    if (amari_vector.end() != p)
+    {
+        for (auto v = p /* mid point*/; v < amari_vector.end(); v++)
+        {
+            repeated.push_back(what_is_shou_for_amari[*v]);
+        }
+    }
 }
 
-int main(int argc, char *argv[])
+// return times
+int repeated_and_amari_cycle(const int M,
+                             /*ref out*/ vector<int> &amari_vector,
+                             /*const ref in*/ const vector<int> &repeated)
 {
-    long long N, M;
-    // std::cin >> N >> M;
-    // std::cin >> N;
-    N = atoi(argv[1]);
-    M = atoi(argv[2]);
-    // M = 3;
-    REP(i, N)
-    {
-        long long di = (pow(10, i) / M);
-        cout << "10**" << i << ", M = " << M << endl;
-        cout << "di: " << di << endl;
-        // cout << "ans: " << di % M << endl;
-    }
+}
 
-    // std::cout << larger(a, b) << std::endl;
+int service(const int N, const int M)
+{
+    vector<int> shou, repeated, what_is_shou_for_amari;
+    vector<int> amari_vector;
+    shou_and_repeated(/*const int*/ M,
+                      /*ref out*/ shou,
+                      /*ref out*/ amari_vector,
+                      /*ref out*/ what_is_shou_for_amari,
+                      /*ref out*/ repeated);
+
+    vector<int> amari_vector_repeated;
+    int times = repeated_and_amari_cycle(
+        /*const int*/ M,
+        /*ref out*/ amari_vector_repeated,
+        /*const ref in*/ repeated);
+
+    // shou を M で割ったあまりはいくつになる？
+    // 結局何桁ある？
+    /*
+        shou を M で割ったあまり
+        => 1 / 88 = 0.01136 + 36 となる。
+        => あまり 80 
+        (消費するNは 5)
+    */
+}
+
+signed main(int argc, char *argv[])
+{
+    long long N;
+    int M;
+    cin >> N >> M;
+
+    service(N, M);
 }
