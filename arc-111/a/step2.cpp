02aa30614224895387a9eb1acfd21c68f767653c
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-10 18:25:35 +0900

    add seperated files

---
+++ arc-111/a/step2.cpp
@@ -0,0 +1,205 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <cstdlib>
+#include <cassert>
+using namespace std;
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define ALL(x) x.begin(), x.end()
+const bool debug = false;
+int pow(int a, int p)
+{
+    if (p == 0)
+        return 1;
+    return a * pow(a, p - 1);
+}
+int get_min_10n(int target)
+{
+    int f = 1;
+    while (f < target)
+    {
+        f *= 10;
+    }
+    return f;
+}
+
+/*
+    STEP 1. まず、1段階目に、 10^N が計算されると、1周りどういうシーケンスが生成されるかを計算する必要がある。
+        最初の方は、ループしない可能性がある。 
+        例: 82 /  6  = 13.66666666 の 13 はループしない。
+        例: 10 /  6 =   1.66666666 の  1 はループしない。
+        -> 10^N を割るときには
+*/
+/*
+    1.666 だから、 どこから始まるか, をメモする必要がある。
+*/
+/*
+    実装できた。あとは引数に結果を渡す。
+*/
+// repeated_string は商 の羅列なので、対応するamari がほしい
+void calc_vec(int M,
+              /*out*/ vector<int> &what_is_shou_for_amari,
+              /*out*/ vector<int> &shou,
+              /*out*/ vector<int> &amari_vector,
+              /*out*/ vector<int> &repeated)
+{
+    set<int> already_amari;
+    int now_num = M;
+    int amari_now = 1;
+    int last_amari = -1;
+
+    what_is_shou_for_amari.resize(M + 1);
+    shou.resize(0);
+    amari_vector.resize(0);
+    repeated.resize(0);
+
+    // ループする 商 を見つける
+    ; // 割り算の商
+    ; // 割り算のあまりの配列
+
+    while (true)
+    {
+        // amari_now は M 以下の状態でここに来る（はず）
+        if (already_amari.find(amari_now) != already_amari.end())
+        {
+            cout << "again amari is : " << amari_now << ", and 商 is " << what_is_shou_for_amari[amari_now] << endl;
+            last_amari = amari_now;
+            break;
+        }
+
+        already_amari.insert(amari_now);
+        amari_vector.push_back(amari_now);
+
+        // ケタあげする。
+        amari_now *= 10;
+
+        // このループ 1回 で 10 を 1回 かける
+        if (amari_now < now_num)
+        {
+            what_is_shou_for_amari[amari_now] = 0;
+            shou.push_back(0);
+            // ここでかける必要はない。
+            continue;
+        }
+
+        shou.push_back(amari_now / M);
+        what_is_shou_for_amari[amari_now / 10] = amari_now / M;
+        amari_now = amari_now % M;
+
+        if (debug)
+        {
+            // cout << "amari_now: " << amari_now << endl;
+        }
+    }
+
+    cout << "result string for 1 / " << M << " = ";
+    int first_num = shou[0] / 10;
+    float first_num_float = (float)shou[0] / 10;
+    if (first_num == 0)
+    {
+        // cout << "0.";
+        if (first_num_float < 0.1)
+        {
+            cout << "0.0";
+        }
+        else
+        {
+            cout << ((float)shou[0] / 10);
+        }
+
+        // cout << first_num;
+    }
+    else
+    {
+        // このケースは M == 1 のときのみ
+        cout << first_num << endl;
+        return;
+    }
+
+    vector<int> new_vec_sub_under_1(shou.begin() + 1, shou.end());
+    for (auto s : new_vec_sub_under_1)
+    {
+        cout << s;
+    }
+    cout << endl;
+    cout << "repeat_string : ";
+    // auto p = .find(last_amari);
+    auto p = std::find(ALL(amari_vector), last_amari);
+    if (amari_vector.end() != p)
+    {
+        for (auto v = p /* mid point*/; v < amari_vector.end(); v++)
+        {
+            int sho_repeated_i = what_is_shou_for_amari[*v];
+            repeated.push_back(sho_repeated_i);
+            cout << sho_repeated_i;
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << "cannot find..";
+    }
+
+    if (debug)
+    {
+        cout << "amari for what is shou for .." << endl;
+        for (auto w : what_is_shou_for_amari)
+        {
+            cout << w << ",";
+        }
+        cout << endl;
+
+        cout << "shou: " << endl;
+        for (auto s : shou)
+        {
+            cout << s << ",";
+        }
+        cout << endl;
+
+        cout << "amari:" << endl;
+        for (auto a : amari_vector)
+        {
+            cout << a << ",";
+        }
+        cout << endl;
+    }
+}
+
+// step 2 では、 実際に10^N / M がどんな形になるかを考える。
+void assert_10_M(int N, int M, const vector<int> &answer)
+{
+    vector<int> what_is_shou_for_amari;
+    vector<int> shou;
+    vector<int> amari_vector;
+    vector<int> repeated;
+
+    // 0 ++ shou が最初の リピート数で、
+    /*
+        1/17 = [0.0588235294117647: [0]++shou ]  repeat部分:[0588235294117647] となるんだけど、
+        [0] ++ shou = 00588235294117647 | 
+        10^N で N==0がデフォルトスタートだから、
+        N = 1 ->     0
+        N = 2 ->     0
+        N = 3 ->     5
+        N = 4 ->    58
+        N = 5 ->   588
+        N = 6 ->  5882
+        という形式になる。
+        最初の長さは、 [0]++shou .size() = 1 + shou.size() で、今回は 17 となる。
+        もし N >= 17 のときは、この部分をつかう
+        repeat.size() = 16 なので、
+        N = 17 未満
+        N = 17 + (16*n) + <アマリ> という形式で表現したい。
+    */
+    calc_vec(M,
+             /*ref out*/ what_is_shou_for_amari,
+             /*ref out*/ shou,
+             /*ref out*/ amari_vector,
+             /*ref out*/ repeated);
+
+    cout << "------------------" << endl;
+}
+
+int main(int argc, char *argv[])
+{
+}
