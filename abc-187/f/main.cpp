7c712d9e703501b94e6507396b70c93c1565b087
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-10 16:10:18 +0900

    update

--- abc-187/f/main.cpp
+++ abc-187/f/main.cpp
@@ -8,8 +8,8 @@
 using namespace std;
 #define ALL(x) x.begin(), x.end()
 #define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
-#define int long long
-
+// #define int long long
+const bool debug = false;
 signed main()
 {
     return 0;
