99a67bb5f73f6283a80644fee6f093a169f713b5
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-11 23:00:03 +0900

    delete test

--- arc-111/a/main.cpp
+++ arc-111/a/main.cpp
@@ -21,18 +21,12 @@ const bool detail_debug = false;
 //         (__builtin_expect(!(e == a), 0) ? __assert_rtn(__func__, __FILE__, __LINE__, #e) : (void)0); \
 //     } while (0)
 /*
+    # やりたいこと
     保存したら make が走るようにしたい。
-*/
-/*
     myassert で赤文字で出したりしたい。
+    環境変数でデバッグON OFF したい。
 */
-// for debug
-/*
-    ❌ 引っかかっているところ
-    1 / 4 などの、循環小数にならずに終わってしまう場合。
-
-    73, 13 などの、最後が
-*/
+/* ---------------------------------------------------------------------------------- */
 void show_vector(/*const ref in*/ const vector<int> &vec, const string &label)
 {
     cout << "[" << label << "]: " << endl;
@@ -90,13 +84,13 @@ void assert_vec(/*const ref in*/ const vector<int> &target, /*const ref in*/ con
         assert(target[i] == answer[i]);
     }
 }
-// ----------
+/* ---------------------------------------------------------------------------------- */
 
-// STEP 1: ✅
+// 普通の循環少数の生成列を作成する。
+// 疑問: 0.250 のようなケースに対しては、どうする？
 void shou_and_repeated(const int M,
                        /*ref out*/ vector<int> &shou,
                        /*ref out*/ vector<int> &amari_vector,
-                       /*ref out*/ vector<int> &what_is_shou_for_amari,
                        /*ref out*/ vector<int> &repeated)
 {
     set<int> already_seen_amari;
@@ -104,7 +98,9 @@ void shou_and_repeated(const int M,
     int amari_now = 1;
     int last_amari = -1;
 
-    what_is_shou_for_amari.resize(M + 1);
+    // local　変数にした。
+    vector<int> what_is_shou_for_amari(M + 1);
+
     shou.resize(0);
     amari_vector.resize(0);
     repeated.resize(0);
@@ -114,10 +110,6 @@ void shou_and_repeated(const int M,
         // amari_now は M 以下の状態でここに来る（はず）
         if (already_seen_amari.find(amari_now) != already_seen_amari.end())
         {
-            if (debug)
-            {
-                cout << "again amari is : " << amari_now << ", and 商 is " << what_is_shou_for_amari[amari_now] << endl;
-            }
             last_amari = amari_now;
             break;
         }
@@ -141,8 +133,6 @@ void shou_and_repeated(const int M,
         what_is_shou_for_amari[amari_now / 10] = amari_now / M;
         amari_now = amari_now % M;
     }
-    // what_is_shou_for_amari, shou の作成。
-    // last_amari が 再度見たあまりとして _ に出てくる。
 
     // repeated の作成
     auto p = std::find(ALL(amari_vector), last_amari);
@@ -153,120 +143,9 @@ void shou_and_repeated(const int M,
             repeated.push_back(what_is_shou_for_amari[*v]);
         }
     }
-
-    // こんなことをする必要はないのでは
-    // if (debug)
-    // {
-    //     show_vector(shou, "shou reset したい");
-    //     show_vector(repeated, "repeated amari");
-    // }
-    // if (shou[shou.size() - 1] == 0 && repeated.size() == 1 and repeated[0] == 0)
-    // {
-
-    //     shou.pop_back();
-    //     if (debug)
-    //     {
-    //         show_vector(shou, "shou reset した");
-    //     }
-    // }
 }
 
-// ✅ checked
-void test_shou_and_repeated()
-{
-    // 88
-    vector<int> shou_88;
-    vector<int> amari_88;
-    vector<int> what_amari_88;
-    vector<int> repeated_88;
-    cout << "--- 88 --- " << endl;
-    shou_and_repeated(88,
-                      /*ref out*/ shou_88,
-                      /*ref out*/ amari_88,
-                      /*ref out*/ what_amari_88,
-                      /*ref out*/ repeated_88
-
-    );
-    show_vector(shou_88, "shou88");
-    show_vector(amari_88, "amari88");
-    show_vector(what_amari_88, "what amari88");
-    show_vector(repeated_88, "repeated_88");
-    assert_vec(shou_88, vector<int>{
-                            0,
-                            1,
-                            1,
-                            3,
-                            6,
-                        });
-    assert_vec(repeated_88, vector<int>{3, 6});
-
-    // 17
-    vector<int> shou_17;
-    vector<int> amari_17;
-    vector<int> what_amari_17;
-    vector<int> repeated_17;
-    cout << "--- 17 --- " << endl;
-    shou_and_repeated(17,
-                      /*ref out*/ shou_17,
-                      /*ref out*/ amari_17,
-                      /*ref out*/ what_amari_17,
-                      /*ref out*/ repeated_17
-
-    );
-    show_vector(shou_17, "shou17");
-    show_vector(amari_17, "amari17");
-    show_vector(what_amari_17, "what amari17");
-    show_vector(repeated_17, "repeated_17");
-    assert_vec(shou_17, vector<int>{0, 5, 8, 8, 2, 3, 5, 2, 9, 4, 1, 1, 7, 6, 4, 7});
-    assert_vec(repeated_17, vector<int>{0, 5, 8, 8, 2, 3, 5, 2, 9, 4, 1, 1, 7, 6, 4, 7});
-
-    // 23
-    vector<int> shou_23;
-    vector<int> amari_23;
-    vector<int> what_amari_23;
-    vector<int> repeated_23;
-    cout << "--- 23 --- " << endl;
-    shou_and_repeated(23,
-                      /*ref out*/ shou_23,
-                      /*ref out*/ amari_23,
-                      /*ref out*/ what_amari_23,
-                      /*ref out*/ repeated_23
-
-    );
-    show_vector(shou_23, "shou23");
-    show_vector(amari_23, "amari23");
-    show_vector(what_amari_23, "what amari23");
-    show_vector(repeated_23, "repeated_23");
-    assert_vec(shou_23, vector<int>{0, 4, 3, 4, 7, 8, 2, 6, 0, 8, 6, 9, 5, 6, 5, 2, 1, 7, 3, 9, 1, 3});
-    assert_vec(repeated_23, vector<int>{
-                                0,
-                                4,
-                                3,
-                                4,
-                                7,
-                                8,
-                                2,
-                                6,
-                                0,
-                                8,
-                                6,
-                                9,
-                                5,
-                                6,
-                                5,
-                                2,
-                                1,
-                                7,
-                                3,
-                                9,
-                                1,
-                                3,
-                            });
-}
-
-// repeated を割るだけ。
-// return amari
-// ✅
+// repeated repeated_with_amari を M で割ったあまりを計算する。
 int repeated_and_amari_cycle(const int M,
                              /*const ref in*/ const vector<int> &repeated_with_amari) // repeated とは限られず、先頭にあまりが付加した形になる。
 {
@@ -279,14 +158,10 @@ int repeated_and_amari_cycle(const int M,
     }
 
     // 提出のときにREした原因 例: eq_assert(service(78, 13), 0, "service(78, 13)");
-    // 0 とは限らない?WA.
+    // 0 とは限らない?
     if (repeated_with_amari.size() == 0)
     {
-        // return
         return 0;
-        //
-        // cout << "cannot be 0 in repeaed" << endl;
-        // assert(false);
     }
 
     int target = repeated_with_amari[i++];
@@ -299,7 +174,6 @@ int repeated_and_amari_cycle(const int M,
             int shou_one = target / M;
             int amari_one = target % M;
             target = amari_one;
-            // shou.push_back(shou_one);
         }
         else
         {
@@ -323,6 +197,7 @@ int repeated_and_amari_cycle(const int M,
     return target;
 }
 
+// 便利関数
 vector<int> num_to_vector(int n)
 {
     vector<int> answer;
@@ -334,44 +209,12 @@ vector<int> num_to_vector(int n)
     reverse(ALL(answer));
     return answer;
 }
-// ✅
-void test_repeated_and_amari_cycle()
-{
-    // 88
-    assert(repeated_and_amari_cycle(88, vector<int>{3, 6}) == 36);
-    assert(repeated_and_amari_cycle(88, vector<int>{3, 6, 3, 6}) == 28);
-    assert(repeated_and_amari_cycle(88, vector<int>{2, 8, 3, 6}) == 20);
-    assert(repeated_and_amari_cycle(88, vector<int>{2, 0, 3, 6}) == 12);
-    assert(repeated_and_amari_cycle(88, vector<int>{4, 3, 6}) == 84);
-    assert(repeated_and_amari_cycle(88, num_to_vector(8436)) == 76);
-    assert(repeated_and_amari_cycle(88, num_to_vector(7636)) == 68);
-    assert(repeated_and_amari_cycle(88, num_to_vector(6836)) == 60);
-}
-// repeated を何回連結させるとあまりがゼロになるか。
-// return times
-// あまりがゼロにならないこともあるのか！！
-// (M=88に対して、 repeated={3,6} だと、同じ(偶数条件) をループしてしまい、ゼロにはならない。)
-/*
-    amari:  4, times: 741889
-    amari: 84, times: 741890
-    amari: 76, times: 741891
-    amari: 68, times: 741892
-    amari: 60, times: 741893
-    amari: 52, times: 741894
-    amari: 44, times: 741895
-    amari: 36, times: 741896
-    amari: 28, times: 741897
-    amari: 20, times: 741898
-    amari: 12, times: 741899
-    amari:  4, times: 741900
-    amari: 84, times: 741901
-*/
-//
-// テスト ✅
+
 // times の代わりに、repeated set に対するあまりの循環を計算する関数
+// これ結構大変じゃない？
 void repeated_mod_times(const int M,
                         const int start_amari,
-                        /*ref out*/ vector<int> &amari_vector,
+                        /*ref out*/ vector<int> &amari_vector, // 名前が悪い気がする。 repeated_amari_repeated とか
                         /*const ref in*/ const vector<int> &repeated)
 {
     int times = 1;
@@ -384,24 +227,10 @@ void repeated_mod_times(const int M,
         start_vec.push_back(r);
     }
 
-    if (debug)
-    {
-        cout << "[mod] repated mod: "
-             << "start amari: " << start_amari << endl;
-        show_vector(start_vec, "start vec"); // 4 の場合、これが1,0 になる。そのため、最初のamari =
-    }
     amari = repeated_and_amari_cycle(/*const int*/ M,
                                      /*const ref in*/ start_vec);
 
-    if (debug)
-    {
-        // show_vector(start_vec, "[repeated mod times] start_vec:");
-        // cout << "amari: " << amari << endl;
-        show_variable(amari, "[repeated mod times] amari");
-    }
     vector<int> not_used_amari_vector;
-    // これを削除した。
-    // amari_vector.push_back(amari);
 
     while (true)
     {
@@ -430,12 +259,11 @@ int service(const int N, const int M)
         cout << "--------- N: " << N << "------ M: " << M << "---------" << endl;
     }
     int _N = N;
-    vector<int> shou, repeated, what_is_shou_for_amari;
+    vector<int> shou, repeated;
     vector<int> amari_vector;
     shou_and_repeated(/*const int*/ M,
                       /*ref out*/ shou,
                       /*ref out*/ amari_vector,
-                      /*ref out*/ what_is_shou_for_amari,
                       /*ref out*/ repeated);
 
     // repeated　をわるあまりの巡回
@@ -536,14 +364,6 @@ int service(const int N, const int M)
     return 1;
 }
 
-void test_service7()
-{
-    eq_assert(service(1, 2), 1, "service(1, 4)"); // 5%2  == 1 => 2 % 4
-    eq_assert(service(2, 2), 0, "service(3, 4)"); // 50%2 == 0
-    eq_assert(service(3, 2), 0, "service(3, 4)");
-    eq_assert(service(4, 2), 0, "service(78, 13)");
-    eq_assert(service(5, 2), 0, "service(78, 13)");
-}
 signed main(signed argc, char *argv[])
 {
     // test_service7();
