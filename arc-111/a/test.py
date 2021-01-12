551913d057232fcd3a3c1e8817a7277d2999b442
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-12 11:45:44 +0900

    update

--- arc-111/a/test.py
+++ arc-111/a/test.py
@@ -5,5 +5,4 @@ m = 10
 for i in range(1, 10):
     str_lst = make_str_lst(m , i)
     with open("/dev/null" , "w") as fout2:
-                    lst_division_for_calc_amari(fout2, str_lst, m)
-# lst_division_for_calc_amari
+        lst_division_for_calc_amari(fout2, str_lst, m)
