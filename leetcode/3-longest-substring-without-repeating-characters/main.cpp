92e8e611f1e3b60f32b7e96629c0df5e1a7f11c8
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-12 13:12:46 +0900

    WIP

--- leetcode/3-longest-substring-without-repeating-characters/main.cpp
+++ leetcode/3-longest-substring-without-repeating-characters/main.cpp
@@ -28,48 +28,65 @@ class Solution {
     set<char> used_char;
     used_char.insert(s[i]);
 
-    // もし、 j が進めるなら進む。
-    // j が s.size() を越してしまった場合は、 ...
-    // s[j] が含まれてしまった場合は、 ...
-    while (j <= s.size() && used_char.find(s[j]) == used_char.end()) {
-      used_char.insert(s[j]);
-      now_score += 1;
-      chmax(now_score, best_answer);
-      j += 1;
-    }
-
-    // j がすぎてしまった場合, もうやる必要はない
-    // (i を動かさなくてもよかったため。)
-    if (j > s.size()) {
-      return best_answer;
-    }
-    if (used_char.find(s[j]) == used_char.end()) {
-      // 上記 の条件がなくなるまで、i を進めてみる。
-      while (i < s.size() && used_char.find(s[j]) == used_char.end()) {
+    // 最初は このループに入れる。
+    // ループ内部では、 i, j の増加がありうる。
+    while (true) {
+      // もし、 j が進めるなら進む。
+      // j が s.size() を越してしまった場合は、 ...
+      // s[j] が含まれてしまった場合は、 ...
+      while (j <= s.size() && used_char.find(s[j]) == used_char.end()) {
+        used_char.insert(s[j]);
+        now_score += 1;
+        chmax(now_score, best_answer);
+        j += 1;
       }
-      if (i == s.size()) {
+      // j がすぎてしまった場合, もうやる必要はない
+      // (i を動かさなくてもよかったため。)
+      if (j > s.size()) {
         return best_answer;
       }
-    }
-  };
-  /* 連続している部分を探す必要がある。 */
-  /* 尺取でいけないか？ */
+      if (used_char.find(s[j]) == used_char.end()) {
+        // 上記 の条件がなくなるまで、i を進めてみる。
+        // ⚡️: i を進めていい条件は j より小さいこと
+        // | 逆に、どういう時に j を越してしまう？
+        while (i < s.size() && used_char.find(s[j]) == used_char.end()) {
+          now_score -= 1;
 
-  void eq_assert(const int val, const int answer, const string &label) {
-    if (val != answer) {
-      cout << "Assertion failed: "
-           << "(val = " << val << ", label = " << label << ", "
-           << ", answer = " << answer << ") " << endl
-           << flush;
-
-      exit(1);
+          // s[i] を 使った set から削除する。
+          assert("s[i] は used なはず。" &&
+                 used_char.find(s[i]) != used_char.end());
+          used_char.erase(used_char.find(s[i]));
+          i += 1;
+        }
+        if (i == s.size()) {
+          return best_answer;
+        }
+        if (used_char.find(s[j]) != used_char.end()) {
+          // もし、 j が入らなくなったら、次へ進んでもよい。
+          // continue したい。
+        }
+      }
     }
   }
+};
+/* 連続している部分を探す必要がある。 */
+/* 尺取でいけないか？ */
 
-  signed main() {
-    Solution a = Solution();
-    eq_assert(a.lengthOfLongestSubstring("abcabcbb"), 3, "abcabcbb");
-    eq_assert(a.lengthOfLongestSubstring("bbbbb"), 1, "bbbbb");
-    eq_assert(a.lengthOfLongestSubstring("pwwkew"), 3, "pwwkew");
-    eq_assert(a.lengthOfLongestSubstring(""), 0, "<empty>");
+void eq_assert(const int val, const int answer, const string &label) {
+  if (val != answer) {
+    cout << "Assertion failed: "
+         << "(val = " << val << ", label = " << label << ", "
+         << ", answer = " << answer << ") " << endl
+         << flush;
+
+    exit(1);
   }
+}
+
+signed main() {
+  Solution a = Solution();
+  eq_assert(a.lengthOfLongestSubstring("abcabcbb"), 3, "abcabcbb");
+  eq_assert(a.lengthOfLongestSubstring("bbbbb"), 1, "bbbbb");
+  eq_assert(a.lengthOfLongestSubstring("pwwkew"), 3, "pwwkew");
+  eq_assert(a.lengthOfLongestSubstring(""), 0, "<empty>");
+}
