2db68914fa8f032307753c6335885054ca1f6dad
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-12 11:02:56 +0900

    delete test from prod code

--- arc-111/a/main2.cpp
+++ arc-111/a/main2.cpp
@@ -151,27 +151,6 @@ void generate_series(const int M,
     }
 }
 
-void test_shou_and_repeated()
-{
-    vector<int> shou_4;
-    vector<int> repeated_4;
-    generate_series(4, shou_4, repeated_4);
-    show_vector(shou_4, "shou_4");         // 2 5 0
-    show_vector(repeated_4, "repeated_4"); // 0
-
-    vector<int> shou_13;
-    vector<int> repeated_13;
-    generate_series(13, shou_13, repeated_13);
-    show_vector(shou_13, "shou_13");         // 0,7,6,9,2,3,
-    show_vector(repeated_13, "repeated_13"); // 0,7,6,9,2,3,
-
-    vector<int> shou_2;
-    vector<int> repeated_2;
-    generate_series(2, shou_2, repeated_2);
-    show_vector(shou_2, "shou_2");         // 5 0
-    show_vector(repeated_2, "repeated_2"); // 0
-}
-
 // repeated repeated_with_amari を M で割ったあまりを計算する。
 int calc_amari_for_target_vec(const int M,
                               /*const ref in*/ const vector<int> &target_vec) // repeated とは限られず、先頭にあまりが付加した形になる。
