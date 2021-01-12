b5d02f4f5af652c1f1b12f4dd31e825155d8e0cf
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-12 11:45:33 +0900

    add test.py py

--- arc-111/a/test.py
+++ arc-111/a/test.py
@@ -1,3 +1,9 @@
-from make_test import lst_division_for_calc_amari
+from make_test import lst_division_for_calc_amari, make_str_lst
 
-lst_division_for_calc_amari
+
+m = 10
+for i in range(1, 10):
+    str_lst = make_str_lst(m , i)
+    with open("/dev/null" , "w") as fout2:
+                    lst_division_for_calc_amari(fout2, str_lst, m)
+# lst_division_for_calc_amari
