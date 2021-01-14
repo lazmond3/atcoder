933b1a503afc9f0d7e417b79318c01cf1c35fa80
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-15 02:03:23 +0900

    init 4 median hard

---
+++ leetcode/4-median/main.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+bool debug = false;
+#define REP(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
+#define ALL(x) x.begin(), x.end()
+#define int long long
+// clang-format off
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+#define RED "\033[1;31m"
+#define GRE "\033[1;32m"
+#define BLU "\033[34m"
+#define CLR "\033[0m"
+// clang-format on
+
+// 🎄⭐️🎅 欲しいもの
+// - string の ゼロフィル関数
+
+/*
+// http://en.wikipedia.org/wiki/ANSI_escape_code
+https://stackoverflow.com/questions/2616906/how-do-i-output-coloured-text-to-a-linux-terminal
+         foreground background
+black        30         40
+red          31         41
+green        32         42
+yellow       33         43
+blue         34         44
+magenta      35         45
+cyan         36         46
+white        37         47
+*/
+
+// https://leetcode.com/problems/median-of-two-sorted-arrays/
+class Solution {
+   public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        return 0.0;
+    }
+};
+
+template <class T>
+void test_eq_assert(const T val, const T answer, const string& label) {
+    if (debug && val == answer) {
+        cout << GRE << "--------- [[ " << label << "]] ---------" << CLR
+             << endl;
+    }
+    if (val != answer) {
+        cout << RED;
+        cout << "--------- [[ " << label << "]] ---------" << endl;
+        cout << CLR;
+        cout << "Assertion failed: "
+             << "(val = " << RED << val << CLR << ", label = " << label << ", "
+             << "answer = " << BLU << answer << CLR << ") " << endl
+             << flush;
+        cout << CLR;
+
+        exit(1);
+    }
+}
+
+signed main() {
+    const char* DEBUG_p = std::getenv("DEBUG");
+    debug = DEBUG_p != NULL && strnlen(DEBUG_p, 1) > 0;
+    Solution a = Solution();
+    // // ❌ 使う部分の範囲だけっぽい x から n文字目だった。 .substr(x, n)
+    // test_eq_assert<string>(string("abcd").substr(1, 3), "bcd",
+    //                        "abcd substr 1,3");
+}
