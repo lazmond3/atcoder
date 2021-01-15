33c4f3cd42bd1bcfb0f0c890e3eb724536d7227e
lazmond3 <moikilo00+xcode@gmail.com>
2021-01-15 17:46:30 +0900

    add files

---
+++ leetcode/4-median/main.hpp
@@ -0,0 +1,11 @@
+#ifndef _mainhpp
+#define _mainhpp
+#include <vector>
+using namespace std;
+extern bool debug;
+class Solution {
+   public:
+    double findMedianSortedArrays(const vector<int>& nums1,
+                                  const vector<int>& nums2);
+};
+#endif
